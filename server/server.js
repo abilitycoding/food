512a42626a41c5723ec210167bccee43b44f5201
ccschoolsedu <ccschools.edu@gmail.com>
2023-09-13 22:05:14 +0530

    Build

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
